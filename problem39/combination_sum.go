b6f6231947546d2406d9b99333358d9d3320682d
mlkr <gatsby.yun@gmail.com>
2019-02-14 11:57:02 +0800

    problem 39 Combination Sum 添加注释

--- problem39/combination_sum.go
+++ problem39/combination_sum.go
@@ -20,6 +20,10 @@ func cs(candidates []int, target int, solution []int, res *[][]int) {
 		return
 	}
 
+	// 这样处理一下的用意是，让切片的容量等于长度，以后append的时候，会分配新的底层数组
+	// 避免多处同时对底层数组进行修改，产生错误的答案。
+	// 可以注释掉以下语句，运行单元测试，查看错误发生。
+	// 两个冒号 cap len 都设置为 len(solution)
 	solution = solution[:len(solution):len(solution)]
 
 	cs(candidates, target-candidates[0], append(solution, candidates[0]), res)
