30d6462ef650dbe1bfec1e8debade15b3b798bc0
mlkr <gatsby.yun@gmail.com>
2019-05-10 09:30:29 +0800

    274 H-Index working

--- problem274/h_index.go
+++ problem274/h_index.go
@@ -1,7 +1,24 @@
 package problem274
 
 func hIndex(citations []int) int {
+	quickSort(citations)
+	left, right := 0, len(citations)-1
 
+	// 从大到小排
+	for left < right {
+		citations[left], citations[right] = citations[right], citations[left]
+		left++
+		right--
+	}
+
+	i := 0
+	for ; i < len(citations); i++ {
+		if i+1 > citations[i] {
+			break
+		}
+	}
+
+	return i
 }
 
 func quickSort(nums []int) {
@@ -10,17 +27,22 @@ func quickSort(nums []int) {
 		return
 	}
 
-	i, j := 1, size-1
+	left, right := 0, size-1
 	base := nums[0]
-	for i <= j {
+	i := 1
+	for i <= right {
 		if nums[i] >= base {
-			nums[i], nums[j] = nums[j], nums[i]
-			j--
+			nums[i], nums[right] = nums[right], nums[i]
+			right--
 		}
 
 		if nums[i] < base {
-			nums[i], nums[0] = nums[i], nums[0]
+			nums[i], nums[left] = nums[left], nums[i]
+			left++
 			i++
 		}
 	}
+
+	quickSort(nums[:left])
+	quickSort(nums[left+1:])
 }
