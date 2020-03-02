a485170629158da84a85433326855bd6a244cf06
mlkr <gatsby.yun@gmail.com>
2020-03-02 22:51:23 +0800

    349 优化

--- problem349/intersection_of_two_arrays.go
+++ problem349/intersection_of_two_arrays.go
@@ -1,21 +1,17 @@
 package problem349
 
 func intersection(nums1 []int, nums2 []int) []int {
-	m1 := make(map[int]struct{})
+	m1 := map[int]bool{}
 	for _, num := range nums1 {
-		m1[num] = struct{}{}
-	}
-
-	m2 := make(map[int]struct{})
-	for _, num := range nums2 {
-		if _, ok := m1[num]; ok {
-			m2[num] = struct{}{}
-		}
+		m1[num] = true
 	}
 
 	res := []int{}
-	for k := range m2 {
-		res = append(res, k)
+	for _, num := range nums2 {
+		if v, ok := m1[num]; ok && v {
+			res = append(res, num)
+			m1[num] = false
+		}
 	}
 
 	return res
