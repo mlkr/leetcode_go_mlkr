fb22e55e8da5674badcf6297d6edfea9b8d9703b
mlkr <gatsby.yun@gmail.com>
2020-03-20 17:23:08 +0800

    363 Max Sum of Rectangle No Larger Than K

--- problem363/max_sum_of_rectangle_no_larger_than_k.go
+++ problem363/max_sum_of_rectangle_no_larger_than_k.go
@@ -10,15 +10,15 @@ func maxSumSubmatrix(matrix [][]int, k int) int {
 	c := len(matrix[0])
 
 	res := math.MinInt64
-	for l:=0;l<c;l++{
+	for l := 0; l < c; l++ {
 
 		rowSum := make([]int, r)
-		for b:=l; b<c;b++{
-			
+		for b := l; b < c; b++ {
+
 			// 计算矩形 acc 的大小
 			acc := 0
 			maxAcc := 0
-			for i:=0;i<r;i++{
+			for i := 0; i < r; i++ {
 				rowSum[i] += matrix[i][b]
 				acc += rowSum[i]
 
@@ -32,7 +32,7 @@ func maxSumSubmatrix(matrix [][]int, k int) int {
 
 				if acc < 0 {
 					acc = 0
-				}else{
+				} else {
 					maxAcc = max(maxAcc, acc)
 				}
 			}
@@ -44,13 +44,13 @@ func maxSumSubmatrix(matrix [][]int, k int) int {
 			// 最大矩形大于 k, 上下剪切
 			sums := []int{}
 			acc = 0
-			for i:=0;i<r;i++{
+			for i := 0; i < r; i++ {
 				acc += rowSum[i]
 
 				sort.Ints(sums)
 				t := sort.SearchInts(sums, acc-k)
 				if t < len(sums) {
-					if acc - sums[t] > res {
+					if acc-sums[t] > res {
 						res = acc - sums[t]
 						if res == k {
 							return res
