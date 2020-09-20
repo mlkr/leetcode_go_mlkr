31022926d504373ae7ae56c1051d8644b6c70693
mlkr <gatsby.yun@gmail.com>
2020-09-20 22:43:48 +0800

    442 添加另一解法

--- problem442/find_all_dublicates_in_an_array.go
+++ problem442/find_all_dublicates_in_an_array.go
@@ -22,3 +22,18 @@ func abs(num int) int {
 
 	return num
 }
+
+func findDuplicates2(nums []int) []int {
+	res := []int{}
+
+	for _, num := range nums {
+		idx := (num & (1<<30 - 1)) - 1
+		if nums[idx]&(1<<30) > 0 {
+			res = append(res, idx+1)
+		}
+
+		nums[idx] |= 1 << 30
+	}
+
+	return res
+}
--- problem442/find_all_dublicates_in_an_array_test.go
+++ problem442/find_all_dublicates_in_an_array_test.go
@@ -6,12 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var questions = []struct{
+var questions = []struct {
 	nums, ans []int
 }{
 	{
-		[]int{4,3,2,7,8,2,3,1},
-		[]int{2,3},
+		[]int{4, 3, 2, 7, 8, 2, 3, 1},
+		[]int{2, 3},
 	},
 }
 
@@ -19,14 +19,38 @@ func Test_findDuplicates(t *testing.T) {
 	ast := assert.New(t)
 
 	for _, q := range questions {
-		ast.Equal(q.ans, findDuplicates(q.nums), "输入 %v", q.nums)
+		nums := make([]int, len(q.nums))
+		copy(nums, q.nums)
+		ast.Equal(q.ans, findDuplicates(nums), "输入 %v", q.nums)
+	}
+}
+
+func Test_findDuplicates2(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, q := range questions {
+		nums := make([]int, len(q.nums))
+		copy(nums, q.nums)
+		ast.Equal(q.ans, findDuplicates2(nums), "输入 %v", q.nums)
 	}
 }
 
 func Benchmark_findDuplicates(b *testing.B) {
-	for i:=0;i<b.N;i++{
+	for i := 0; i < b.N; i++ {
 		for _, q := range questions {
-			findDuplicates(q.nums)
+			nums := make([]int, len(q.nums))
+			copy(nums, q.nums)
+			findDuplicates(nums)
+		}
+	}
+}
+
+func Benchmark_findDuplicates2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, q := range questions {
+			nums := make([]int, len(q.nums))
+			copy(nums, q.nums)
+			findDuplicates2(nums)
 		}
 	}
 }
